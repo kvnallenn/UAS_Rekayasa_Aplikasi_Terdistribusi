e31c4f89b03f61ac93a5cfb5ce4433b0e0177ce1
Kevin Allen <62019706+kvnallenn@users.noreply.github.com>
2021-05-03 23:23:52 +0700

    Add files via upload

---
+++ mahasiswa.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 03, 2021 at 06:23 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datamahasiswa`
+--
+
+CREATE TABLE `datamahasiswa` (
+  `id` int(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `umur` int(100) NOT NULL,
+  `nim` varchar(20) NOT NULL,
+  `alamat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `datamahasiswa`
+--
+
+INSERT INTO `datamahasiswa` (`id`, `nama`, `umur`, `nim`, `alamat`) VALUES
+(7, 'Allen ketigaWDA', 812, '0308272723', 'Jln. TribunNN'),
+(11, 'abc3131', 90, '01', 'wadwawdwa');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `datamahasiswa`
+--
+ALTER TABLE `datamahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `datamahasiswa`
+--
+ALTER TABLE `datamahasiswa`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
